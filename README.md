635a76419c5e173aa05120b2459fd0c4c6f55dee
BrerBearFan534 <holdencampbellmoore@outlook.com>
2025-06-03 09:41:50 -0400

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://brerbearfan534.github.io/killer-clown-man/](https://brerbearfan534.github.io/killer-clown-man/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/brerbearfan534/killer-clown-man** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/brerbearfan534/killer-clown-man** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Clown":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . 2 . . . . . . . . . 
+. . . . . 2 2 2 . . . . . . . . 
+. 2 . . 1 1 1 1 1 . . 2 . . . . 
+. 2 2 2 1 f 1 f 1 2 2 2 . . . . 
+. . 2 2 1 1 2 1 1 2 2 . . . . . 
+. . . . 1 f 1 f 1 . . . . . . . 
+. . . . 1 1 f 1 1 . . . . . . . 
+4 4 f 4 4 4 4 4 4 4 f 4 4 . . . 
+4 4 f 4 4 4 4 4 4 4 f 4 4 . . . 
+4 4 f 4 4 4 4 4 4 4 f 4 4 . . . 
+4 4 f 4 4 4 4 4 4 4 f 4 4 . . . 
+4 4 f 4 4 4 4 4 4 4 f 4 4 . . . 
+1 1 . 4 4 4 . 4 4 4 . 1 1 . . . 
+. . . 4 4 4 . 4 4 4 . . . . . . 
+. . . 4 4 4 . 4 4 4 . . . . . . 
+`;
+            case "image2":
+            case "Knife":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . f f f f b b b 2 2 . . . 
+. . . . f f f f b b b 2 . . . . 
+. . . . . . . f . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
